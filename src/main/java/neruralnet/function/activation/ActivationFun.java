2fbdfcd2904605d07cd4a4163e4b332b4f36a998
Leon Rotim <leonrotim@gmail.com>
2015-12-08 22:34:36 +0100

    tang hip

--- src/main/java/neruralnet/function/activation/ActivationFun.java
+++ src/main/java/neruralnet/function/activation/ActivationFun.java
@@ -1,8 +1,12 @@
 package neruralnet.function.activation;
 
 public interface ActivationFun {
-	
+
 	public static ActivationFun sigmoid = (x) -> 1. / (1 + Math.pow(Math.E, -x));
-	
+	public static ActivationFun tangh = (x) -> {
+		double tmp = Math.pow(Math.E, -2 * x);
+		return (1. - tmp) / (1. + tmp);
+	};
+
 	public double apply(double value);
 }
